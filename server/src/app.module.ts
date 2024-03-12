eb0fd21a99cf4d87876069755a79d857071bcedd
DmitriiTMS <dmitrii12_88@mail.ru>
2024-03-12 20:43:16 +0300

    add mysql

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -18,19 +18,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
+        type: 'mysql',
+        // type: 'postgres',
         host: configService.get('DB_HOST'),
         port: configService.get('DB_PORT'),
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
         synchronize: true,
-        entities: [`${__dirname}/**/*.entity{.js, .ts}`]
+        entities: [`${__dirname}/**/*.entity{.js, .ts}`],
       }),
-      inject: [ConfigService]
-    })
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
