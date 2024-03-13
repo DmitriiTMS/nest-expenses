b84abe6736201471abd4589a3f1dad520f114df5
DmitriiTMS <dmitrii12_88@mail.ru>
2024-03-13 16:01:59 +0300

    add register start

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserDto {}
+import { IsEmail, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsEmail({}, {message: 'Введите корректный email'})
+    email: string;
+
+    @MinLength(3, {message: 'Пароль должен быть более 3 символов'})
+    password: string;
+}
